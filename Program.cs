9df3b12c2be93f72b9e7627e2005fa34187dbf51
Duom1 <you@example.com>
2026-03-17 18:30:37 +0200

    added serialization methods to Cube class

--- Program.cs
+++ Program.cs
@@ -310,6 +310,16 @@ namespace rubiks_cube_simulator
             return _blocks[index];
         }
 
+        public void serialize(string fileName)
+        {
+            string h = JsonSerializer.Serialize(this);
+            // Console.WriteLine(h);
+            using (StreamWriter f = new StreamWriter(fileName))
+            {
+                f.Write(h);
+            }
+        }
+
         public bool compare(Cube cube)
         {
             for (int i = 0; i < _blocks.Length; ++i)
@@ -387,6 +397,20 @@ namespace rubiks_cube_simulator
         }
 
         public Cube() { }
+
+        public Cube(string fileName)
+        {
+            string h = "";
+            using (StreamReader f = new StreamReader(fileName))
+            {
+                h = f.ReadToEnd();
+            }
+            Cube? c = JsonSerializer.Deserialize<Cube>(h);
+            if (c != null)
+            {
+                this.blocks = c.blocks;
+            }
+        }
 
         public Cube(Boolean createNew = true)
         {
@@ -841,38 +865,47 @@ namespace rubiks_cube_simulator
         static void Main(string[] args)
         {
             Cube cube = new Cube(true);
-            Cube cube2 = new Cube(true);
-            Cube? cube3 = new Cube();
-            Console.WriteLine(cube.compare(cube2));
+            Cube cube2 = new Cube("./tmp/test.json");
+
+            while (!cube.compare(cube2))
+            {
+                cube2.doAlgo("RUR'URUUR'");
+                cube2.print();
+            }
+
+            // Cube cube = new Cube(true);
+            // Cube cube2 = new Cube(true);
+            // Cube? cube3 = new Cube();
+            // Console.WriteLine(cube.compare(cube2));
             // cube.doAlgo("RUR'URUUR'");
-            cube.print();
-            string h = JsonSerializer.Serialize(cube);
-            Console.WriteLine(h);
-            using (StreamWriter f = new StreamWriter("./tmp/test.json"))
-            {
-                f.Write(h);
-            }
-
-            foreach (var item in cube.blocks)
-            {
-                Console.WriteLine(item?.GetType());
-            }
-
-            cube3 = JsonSerializer.Deserialize<Cube>(h);
-            h = JsonSerializer.Serialize(cube3);
-            Console.WriteLine(h);
-            using (StreamWriter f = new StreamWriter("./tmp/test2.json"))
-            {
-                f.Write(h);
-            }
-            if (cube3 != null)
-            {
-                foreach (var item in cube3.blocks)
-                {
-                    Console.WriteLine(item?.GetType());
-                }
-                cube3.print();
-            }
+            // cube.print();
+            // string h = JsonSerializer.Serialize(cube);
+            // Console.WriteLine(h);
+            // using (StreamWriter f = new StreamWriter("./tmp/test.json"))
+            // {
+            //     f.Write(h);
+            // }
+            //
+            // foreach (var item in cube.blocks)
+            // {
+            //     Console.WriteLine(item?.GetType());
+            // }
+            //
+            // cube3 = JsonSerializer.Deserialize<Cube>(h);
+            // h = JsonSerializer.Serialize(cube3);
+            // Console.WriteLine(h);
+            // using (StreamWriter f = new StreamWriter("./tmp/test2.json"))
+            // {
+            //     f.Write(h);
+            // }
+            // if (cube3 != null)
+            // {
+            //     foreach (var item in cube3.blocks)
+            //     {
+            //         Console.WriteLine(item?.GetType());
+            //     }
+            //     cube3.print();
+            // }
 
             // Cube? cube;
             // using (StreamReader r = new StreamReader("./tmp/test.json"))
